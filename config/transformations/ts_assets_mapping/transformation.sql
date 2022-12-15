ed67482b8fa21a4420dad9725c5a1704f2bfdc9b
JulietaAlvarez92 <jmartinez229@slb.com>
2022-12-15 08:34:31 +0000

    DONE

--- config/transformations/ts_assets_mapping/transformation.sql
+++ config/transformations/ts_assets_mapping/transformation.sql
@@ -1,1 +1,4 @@
-#tihs is a comment
+SELECT ts.externalId AS externalId,
+       ast.id AS assetId
+FROM `_cdf`.`timeseries` AS ts
+INNER JOIN `_cdf`.`assets` AS ast ON split(ts.externalId, ":") [0] = ast.externalId
